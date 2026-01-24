a00a4bbc1f98dabd6c66f6789079e21835d804b2
Jiangliangtong <jiangliangtong@gmail.com>
2026-01-24 17:47:16 +0800

    feat: init

--- lib/services/douban_service.dart
+++ lib/services/douban_service.dart
@@ -1,10 +1,13 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/movie.dart';
+import 'config_service.dart';
 
-final doubanServiceProvider = Provider((ref) => DoubanService());
+final doubanServiceProvider = Provider((ref) => DoubanService(ref));
 
 class DoubanService {
+  final Ref _ref;
+
   final Dio _dio = Dio(BaseOptions(
     headers: {
       'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Mobile/15E148 Safari/604.1',
@@ -12,17 +15,28 @@ class DoubanService {
     },
   ));
 
-  String getDoubanBase(String subdomain) {
-    // 恢复使用豆瓣镜像域名，确保在非 Web 环境下的稳定性
-    return 'https://$subdomain.douban.cmliussss.net';
+  DoubanService(this._ref);
+
+  Future<String> getDoubanBase(String subdomain) async {
+    final configService = _ref.read(configServiceProvider);
+    final proxyType = await configService.getDoubanProxyType();
+
+    switch (proxyType) {
+      case 'tencent-cmlius':
+        return 'https://$subdomain.douban.cmliussss.net';
+      case 'direct':
+        return 'https://$subdomain.douban.com';
+      default:
+        return 'https://$subdomain.douban.cmliussss.net';
+    }
   }
 
   Future<DoubanSubject?> getDetail(String id) async {
     try {
-      final baseUrl = getDoubanBase('m');
+      final baseUrl = await getDoubanBase('m');
       final response = await _dio.get('$baseUrl/rexxar/api/v2/movie/$id');
       final data = response.data;
-      
+
       return DoubanSubject(
         id: data['id'].toString(),
         title: data['title'] ?? '',
@@ -35,7 +49,7 @@ class DoubanService {
     } catch (e) {
       // Fallback for TV series
       try {
-        final baseUrl = getDoubanBase('m');
+        final baseUrl = await getDoubanBase('m');
         final response = await _dio.get('$baseUrl/rexxar/api/v2/tv/$id');
         final data = response.data;
         return DoubanSubject(
@@ -55,7 +69,7 @@ class DoubanService {
 
   Future<List<DoubanSubject>> getList(String type, {String tag = '热门', int pageStart = 0}) async {
     try {
-      final baseUrl = getDoubanBase('movie');
+      final baseUrl = await getDoubanBase('movie');
       final response = await _dio.get('$baseUrl/j/search_subjects', queryParameters: {
         'type': type,
         'tag': tag,
@@ -72,7 +86,7 @@ class DoubanService {
 
   Future<List<DoubanSubject>> getRexxarList(String kind, String category, String type, {int pageStart = 0}) async {
     try {
-      final baseUrl = getDoubanBase('m');
+      final baseUrl = await getDoubanBase('m');
       final response = await _dio.get('$baseUrl/rexxar/api/v2/subject/recent_hot/$kind', queryParameters: {
         'start': pageStart,
         'count': 24,
@@ -89,7 +103,7 @@ class DoubanService {
 
   Future<List<DoubanSubject>> getRecommendList(String kind, Map<String, String> filters, {int pageStart = 0}) async {
     try {
-      final baseUrl = getDoubanBase('m');
+      final baseUrl = await getDoubanBase('m');
       final tags = <String>[];
       final selectedCategories = <String, String>{};
 
